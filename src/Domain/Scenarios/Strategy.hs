9fa85b21babd59c2aae2100e94fdc9957ff4a157
Marcell van Geest <marcell@marcell.nl>
2013-11-07 19:02:13 +0100

    Added strategy cache

--- src/Domain/Scenarios/Strategy.hs
+++ src/Domain/Scenarios/Strategy.hs
@@ -4,6 +4,9 @@ import Ideas.Common.Library
 import Domain.Scenarios.State
 import Domain.Scenarios.Parser
 import Control.Monad
+import qualified Data.Map as M
+
+type StrategyMap a = M.Map String (Strategy a)
 
 guardedRule :: IsId b => b -> String -> (a -> Bool) -> (a -> a) -> Rule a
 guardedRule identifier description check update =
@@ -12,18 +15,32 @@ guardedRule identifier description check update =
 makeStrategy :: Monad m => Script -> m (Strategy State)
 makeStrategy script = do
     startId <- getScriptStartId script
-    makeSubStrategy script startId
+    (fullStrategy, _) <- makeSubStrategy script M.empty startId
+    return fullStrategy
 
-makeSubStrategy :: Monad m => Script -> String -> m (Strategy State)
-makeSubStrategy script statementId = do
+makeSubStrategy :: Monad m => Script -> StrategyMap State -> String -> m (Strategy State, StrategyMap State)
+makeSubStrategy script strategyMap statementId = do
     scriptId <- getScriptId script
     statement <- findStatement script statementId
     statementDescription <- getText statement
-    let rule = guardedRule
-            ("scenarios." ++ scriptId ++ "." ++ statementId)
-            (statementDescription)
-            (const true)
-            (id)
-    nextIds <- getNexts statement
-    nexts <- mapM (makeSubStrategy script) nextIds
-    return $ if null nexts then atomic rule else rule <*> (foldr1 (<|>) nexts)
+    case M.lookup statementId strategyMap of
+        Just statementStrategy -> return (statementStrategy, strategyMap)
+        Nothing -> do
+            let rule = guardedRule
+                    ("scenarios." ++ scriptId ++ "." ++ statementId)
+                    (statementDescription)
+                    (const true)
+                    (id)
+            nextIds <- getNexts statement
+            case nextIds of
+                []                        -> do
+                    let statementStrategy = atomic rule
+                    return (statementStrategy, M.insert statementId statementStrategy strategyMap)
+                firstNextId : restNextIds -> do
+                    firstStrategyTuple <- makeSubStrategy script strategyMap firstNextId
+                    (foldedStrategy, nextsStrategyMap) <- foldM folder firstStrategyTuple restNextIds
+                    let statementStrategy = rule <*> foldedStrategy
+                    return (statementStrategy, M.insert statementId statementStrategy nextsStrategyMap)
+    where folder (stratSoFar, rulesSoFar) nextId = do
+            (newStrat, newRules) <- makeSubStrategy script rulesSoFar nextId
+            return (stratSoFar <|> newStrat, newRules) 
