adfc3109441c16b2e3a0ab0453df7cce4916f841
prof1990 <prof1990@hotmail.com>
2014-03-07 16:49:07 +0100

    Finished the build script

---
+++ build.bat
@@ -0,0 +1,17 @@
+set outputfileA="../_output/ideas/"
+set outputfileB="../_output/scriptman/"
+set ghcFlags=--make -static -optl-static -O2 -fforce-recomp -hidir "../_output/intermediate" -odir "../_output/intermediate" -stubdir ../_output/intermediate
+
+if EXIST "./_output" rmdir /s/q "./_output"
+
+mkdir "./_output"
+cd "./_output"
+mkdir ideas
+mkdir scriptman
+mkdir intermediate
+cd ../
+cd ./ScriptManServer
+ghc %ghcFlags% -o ../_output/scriptman/Main.cgi "./Main.hs"
+cd ../src
+ghc %ghcFlags% -o ../_output/ideas/Main.cgi "./Main.hs"
+cd ../
