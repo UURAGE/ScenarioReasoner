1122a2f29901446bacb3757edd6811659ae32b03
Marcell van Geest <marcell@marcell.nl>
2016-02-10 19:08:21 +0100

    Added command-line mode to ScenarioParser

--- src/ScenarioParser.hs
+++ src/ScenarioParser.hs
@@ -13,16 +13,25 @@ import Network.CGI
 
 import Domain.Scenarios.Parser
 
+import System.Environment
 import System.FilePath
 
 main :: IO ()
-main = runCGI $ handleErrors $ do
-    script_path <- getInput "script_path"
-    script <- liftIO $ parseScript (fromJust script_path)
+main = do
+    args <- getArgs
+    case args of
+        [script_path, bin_path] -> process script_path bin_path
+        _ -> runCGI $ handleErrors $ do
+            script_path <- getInput "script_path"
+            bin_path <- getInput "bin_path"
+            process (fromJust script_path) (fromJust bin_path)
+            output (fromJust bin_path)
+
+process :: MonadIO m => FilePath -> FilePath -> m ()
+process script_path bin_path = do
+    script <- liftIO $ parseScript script_path
     let scenario = parseScenario script
-    bin_path <- getInput "bin_path"
-    liftIO $ encodeFile (fromJust bin_path) scenario
-    output (fromJust bin_path)
+    liftIO $ encodeFile bin_path scenario
     
 debugMain :: String -> IO ()
 debugMain path = do
