518232a53d306de16dedb13a96eea8785230e014
Jordy van Dortmont <ydroj@live.nl>
2016-03-29 21:11:19 +0200

    Adjusted copyright notice

--- src/Domain/Scenarios/Condition.hs
+++ src/Domain/Scenarios/Condition.hs
@@ -1,9 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
-------------------------------------------------------------------------------------
--- This program has been developed by students from the bachelor Computer Science
--- at Utrecht University within the Software and Game project course (2013-2015)
--- ©Copyright Utrecht University (Department of Information and Computing Sciences)
-------------------------------------------------------------------------------------
+{- ©Copyright Utrecht University (Department of Information and Computing Sciences) -}
 
 module Domain.Scenarios.Condition where
 
