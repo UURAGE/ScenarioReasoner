270fd789bbda450c2c3b95e1233aded0a985ea81
Marcell van Geest <marcell@marcell.nl>
2016-05-25 23:39:50 +0200

    Implemented ID-returning command-line mode, fixed encoding issues
    When passed the "-r" command-line option, the ScenarioParser will return
    the ID that the ScenarioReasoner will use to refer to the scenario just
    parsed. Various encoding issues have been fixed.

--- src/ScenarioParser.hs
+++ src/ScenarioParser.hs
@@ -7,31 +7,42 @@
 module Main where
 
 import Data.Binary
+import Data.Functor
 import Data.Maybe
 
 import Network.CGI
+
+import Ideas.Common.Id
+import qualified Ideas.Text.UTF8 as UTF8
 
 import Domain.Scenarios.Parser
 
 import System.Environment
 import System.FilePath
+import System.IO
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [script_path, bin_path] -> process script_path bin_path
+        ["-r", script_path, bin_path] -> do
+            scenarioID <- process script_path bin_path
+            hSetEncoding stdout utf8
+            putStrLn scenarioID
+        [script_path, bin_path] -> void (process script_path bin_path)
         _ -> runCGI $ handleErrors $ do
             script_path <- getInput "script_path"
             bin_path <- getInput "bin_path"
-            process (fromJust script_path) (fromJust bin_path)
-            output (fromJust bin_path)
+            scenarioID <- process (fromJust script_path) (fromJust bin_path)
+            setHeader "Content-Type" "text/plain; charset=utf-8"
+            output (UTF8.encode scenarioID)
 
-process :: MonadIO m => FilePath -> FilePath -> m ()
+process :: MonadIO m => FilePath -> FilePath -> m String
 process script_path bin_path = do
     script <- liftIO $ parseScript script_path
     let scenario = parseScenario script
     liftIO $ encodeFile bin_path scenario
+    return (showId (newId (takeBaseName bin_path)))
 
 debugMain :: String -> IO ()
 debugMain path = do
